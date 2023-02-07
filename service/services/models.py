168b3bc337948b73212557a28563d50984e5a0c5
Yunus <gadamurrr@gmail.com>
2023-02-08 00:12:22 +0300

    created_models

---
+++ service/services/models.py
@@ -0,0 +1,27 @@
+from django.core.validators import MaxValueValidator
+from django.db import models
+
+from clients.models import Client
+
+
+class Service(models.Model):
+    name = models.CharField(max_length=50)
+    full_price = models.PositiveIntegerField()
+
+
+class Plan(models.Model):
+    PLAN_TYPES = (
+        ('full', 'Full'),
+        ('student', 'Student'),
+        ('discount', 'Discount')
+    )
+    plan_type = models.CharField(max_length=10, choices=PLAN_TYPES)
+    discount = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(100)])
+
+
+class Subscription(models.Model):
+    # related_name - под каким именем будет доступна внутри модели с которой создаем связь Client
+    client = models.ForeignKey(Client, related_name='subscriptions', on_delete=models.PROTECT)
+    service = models.ForeignKey(Service, related_name='subscriptions', on_delete=models.PROTECT)
+    plan = models.ForeignKey(Plan, related_name='subscriptions', on_delete=models.PROTECT)
+
