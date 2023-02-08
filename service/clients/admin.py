784e3ad9e5c572e824325c044693d68e2737d545
Yunus <gadamurrr@gmail.com>
2023-02-08 15:20:13 +0300

    app_&_routes-4

--- service/clients/admin.py
+++ service/clients/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from clients.models import *
+
+
+admin.site.register(Client)
--- service/services/models.py
+++ service/services/models.py
@@ -8,6 +8,9 @@ class Service(models.Model):
     name = models.CharField(max_length=50)
     full_price = models.PositiveIntegerField()
 
+    def __str__(self):
+        return f'Service: {self.name}'
+
 
 class Plan(models.Model):
     PLAN_TYPES = (
@@ -18,10 +21,16 @@ class Plan(models.Model):
     plan_type = models.CharField(max_length=10, choices=PLAN_TYPES)
     discount = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(100)])
 
+    def __str__(self):
+        return f'Plan: {self.plan_type}'
+
 
 class Subscription(models.Model):
     # related_name - под каким именем будет доступна внутри модели с которой создаем связь Client
     client = models.ForeignKey(Client, related_name='subscriptions', on_delete=models.PROTECT)
     service = models.ForeignKey(Service, related_name='subscriptions', on_delete=models.PROTECT)
     plan = models.ForeignKey(Plan, related_name='subscriptions', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f'Sub - Client: {self.client}, Service: {self.service}, Plan: {self.plan}'
 
