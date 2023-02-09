02ac231e579de93497eb233109bc24e82f11f49e
Yunus <gadamurrr@gmail.com>
2023-02-09 21:34:03 +0300

    optimization & tasks

--- service/services/models.py
+++ service/services/models.py
@@ -2,6 +2,7 @@ from django.core.validators import MaxValueValidator
 from django.db import models
 
 from clients.models import Client
+from services.tasks import set_price
 
 
 class Service(models.Model):
@@ -10,6 +11,17 @@ class Service(models.Model):
 
     def __str__(self):
         return f'Service: {self.name}'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__full_price = self.full_price
+
+    def save(self, *args, **kwargs):
+        if self.full_price != self.__full_price:
+            for subscription in self.subscriptions.all():
+                set_price.delay(subscription.id)
+
+        return super().save(*args, **kwargs)
 
 
 class Plan(models.Model):
@@ -24,13 +36,30 @@ class Plan(models.Model):
     def __str__(self):
         return f'Plan: {self.plan_type}'
 
+    # при создании плана таска все равно будет запускаться
+    # а нужно сделать так, чтобы она запускалась ТОЛЬКО при изменении плана
+    # поэтому: переопределим инициализацию модели
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__discount = self.discount
+    # в момент ее инициализации нужно зафиксировать пару значений
+
+    # сохранять будем тут, т.к. от ПЛАНА могут зависеть несколько подписок
+    def save(self, *args, **kwargs):
+        #                       related_name для плана (из подписки)
+        if self.discount != self.__discount:
+            for subscription in self.subscriptions.all():
+                set_price.delay(subscription.id)
+
+        return super().save(*args, **kwargs)
+
 
 class Subscription(models.Model):
     # related_name - под каким именем будет доступна внутри модели с которой создаем связь Client
     client = models.ForeignKey(Client, related_name='subscriptions', on_delete=models.PROTECT)
     service = models.ForeignKey(Service, related_name='subscriptions', on_delete=models.PROTECT)
     plan = models.ForeignKey(Plan, related_name='subscriptions', on_delete=models.PROTECT)
+    price = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return f'Sub - Client: {self.client}, Service: {self.service}, Plan: {self.plan}'
-
--- service/services/views.py
+++ service/services/views.py
@@ -10,13 +10,7 @@ class SubscriptionView(ReadOnlyModelViewSet):
     queryset = Subscription.objects.all().prefetch_related(
         'plan',
         Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name', 'user__email'))
-    ).annotate(price=F('service__full_price') -
-                     F('service__full_price') * F('plan__discount') / 100.00)
-    # теперь в queryset каждый результат будет дополнен именем price
-    # .annotate - вычисления на уровне БД, чтобы не добавлять еще один prefetch
-    # у каждой подписки появится вирутальное поле price как и в случае с SerializerMethodField()
-    # F - функция, с помощью которой обраащемся к полям
-
+    )
     serializer_class = SubscriptionSerializer
 
     def list(self, request, *args, **kwargs):
